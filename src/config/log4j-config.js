bf9ab180651ee6e578726fe25e6eb30d9591ff53
liuss <liuss@visionet.com.cn>
2017-10-31 18:33:50 +0800

    更新log4js

--- src/config/log4j-config.js
+++ src/config/log4j-config.js
@@ -8,13 +8,14 @@ const log4jConfig = require('./log4j-config.json');
  * log4j-config.json 中 filename 不能以 / 开头
  * 配置文件中的 log_path 要么是绝对路径要么是相对项目根目录 或者什么都不填
  */
-log4jConfig.appenders.forEach(function (appender) {
+Object.keys(log4jConfig.appenders).forEach(function (key) {
+  let appender = log4jConfig.appenders[key];
   if (!appender.filename) return;
 
   let separatorIndex = appender.filename.lastIndexOf('/');
   if (separatorIndex != -1) {
     let arr = Array.from(appender.filename);
-    arr.splice(separatorIndex + 1, 0, config.log_prefix + '_');
+    arr.splice(separatorIndex + 1, 0, 'logic_' + config.port + '_');
     appender.filename = arr.join('');
     if (config.log_path) {//默认路径为程序入口主文件所在目录
       appender.filename = path.resolve(config.log_path + '/' + appender.filename);
